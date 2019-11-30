1d5f4a7e95cf5e53da744dc05817b1bc4083f60e
William <whill4@me.com>
2019-11-30 20:18:18 +0200

    Added website code

---
+++ Gemfile
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+
+# gem "rails"
+gem 'jekyll-autoprefixer'
+
+# If you have any plugins, put them here!
+group :jekyll_plugins do
+  gem "github-pages"
+end
+
+# Windows does not include zoneinfo files, so bundle the tzinfo-data gem
+gem "tzinfo-data", platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+# Performance-booster for watching directories on Windows
+gem "wdm", "~> 0.1.0" if Gem.win_platform?
+
---
+++ assets/js/barba.js
@@ -0,0 +1,172 @@
+window.addEventListener("load", () => {
+  Barba.Pjax.start();
+
+  var slide_left = Barba.BaseTransition.extend({
+    start: function() {
+      Promise.all([this.newContainerLoading, this.fadeOut()]).then(
+        this.fadeIn.bind(this)
+      );
+    },
+
+    fadeOut: function() {
+      // don't want anything to happen because the current page will be replaced - "slid over"
+      var slideType = "slide-out-left";
+      this.oldContainer.classList.add(slideType);
+
+      var that = this;
+
+      this.oldContainer.addEventListener("animationend", function() {
+        that.oldContainer.classList.remove(slideType);
+        that.done();
+      });
+    },
+
+    fadeIn: function() {
+      var slideType = "slide-left";
+      this.newContainer.classList.add(slideType);
+
+      var that = this;
+
+      this.newContainer.addEventListener("animationend", function() {
+        that.newContainer.classList.remove(slideType);
+        that.done();
+      });
+      startTimer();
+    }
+  });
+  var slide_right = Barba.BaseTransition.extend({
+    start: function() {
+      Promise.all([this.newContainerLoading, this.fadeOut()]).then(
+        this.fadeIn.bind(this)
+      );
+    },
+
+    fadeOut: function() {
+      // don't want anything to happen because the current page will be replaced - "slid over"
+      var slideType = "slide-out-right";
+      this.oldContainer.classList.add(slideType);
+
+      var that = this;
+
+      this.oldContainer.addEventListener("animationend", function() {
+        that.oldContainer.classList.remove(slideType);
+        that.done();
+      });
+    },
+
+    fadeIn: function() {
+      var slideType = "slide-right";
+      this.newContainer.classList.add(slideType);
+
+      var that = this;
+
+      this.newContainer.addEventListener("animationend", function() {
+        that.newContainer.classList.remove(slideType);
+        that.done();
+      });
+      startTimer();
+    }
+  });
+  var slide_up = Barba.BaseTransition.extend({
+    start: function() {
+      Promise.all([this.newContainerLoading, this.fadeOut()]).then(
+        this.fadeIn.bind(this)
+      );
+    },
+
+    fadeOut: function() {
+      // don't want anything to happen because the current page will be replaced - "slid over"
+      var slideType = "slide-out-up";
+      this.oldContainer.classList.add(slideType);
+
+      var that = this;
+
+      this.oldContainer.addEventListener("animationend", function() {
+        that.oldContainer.classList.remove(slideType);
+        that.done();
+      });
+    },
+
+    fadeIn: function() {
+      var slideType = "slide-up";
+      this.newContainer.classList.add(slideType);
+
+      var that = this;
+
+      this.newContainer.addEventListener("animationend", function() {
+        that.newContainer.classList.remove(slideType);
+        that.done();
+      });
+      startTimer();
+    }
+  });
+  var slide_down = Barba.BaseTransition.extend({
+    start: function() {
+      Promise.all([this.newContainerLoading, this.fadeOut()]).then(
+        this.fadeIn.bind(this)
+      );
+    },
+
+    fadeOut: function() {
+      // don't want anything to happen because the current page will be replaced - "slid over"
+      var slideType = "slide-out-down";
+      this.oldContainer.classList.add(slideType);
+
+      var that = this;
+
+      this.oldContainer.addEventListener("animationend", function() {
+        that.oldContainer.classList.remove(slideType);
+        that.done();
+      });
+    },
+
+    fadeIn: function() {
+      var slideType = "slide-down";
+      this.newContainer.classList.add(slideType);
+
+      var that = this;
+
+      this.newContainer.addEventListener("animationend", function() {
+        that.newContainer.classList.remove(slideType);
+        that.done();
+      });
+      startTimer();
+    }
+  });
+  /**
+   * Next step, you have to tell Barba to use the new Transition
+   */
+
+  Barba.Pjax.getTransition = function() {
+    // will have to hardcode which transition to use based on the current html page and the new html page
+    var prev = Barba.HistoryManager.prevStatus()
+      .url.split("/")
+      .pop();
+    var next = Barba.HistoryManager.currentStatus()
+      .url.split("/")
+      .pop();
+
+    if (prev == "" || prev == "index.html") {
+      if (next == "menu.html") return slide_left;
+      else if (next == "gallery.html") return slide_right;
+      else if (next == "contact.html") return slide_up;
+      else if (next == "about.html") return slide_down;
+    } else if (prev == "about.html") {
+      if (next == "index.html") return slide_up;
+      else if (next == "menu.html") return slide_left;
+      else if (next == "gallery.html") return slide_right;
+    } else if (prev == "contact.html") {
+      if (next == "index.html") return slide_down;
+      else if (next == "menu.html") return slide_left;
+      else if (next == "gallery.html") return slide_right;
+    } else if (prev == "gallery.html") {
+      if (next == "index.html") return slide_left;
+      else if (next == "contact.html") return slide_up;
+      else if (next == "about.html") return slide_down;
+    } else if (prev == "menu.html") {
+      if (next == "index.html") return slide_right;
+      else if (next == "contact.html") return slide_up;
+      else if (next == "about.html") return slide_down;
+    }
+  };
+});
